29731cc4a4763e8ac3ad81158bd2c234604a7f77
Youssouf A. Toure <toureyoussouf@outlook.it>
2023-01-14 21:17:17 +0100

    -COMPLETATO 90% -sistemare endl dimenticati -aggiornare struct dopo funzione MODIFICA -cercare modo per pulire schermo

--- vocabolario.cpp
+++ vocabolario.cpp
@@ -7,6 +7,7 @@ VOCABOLARIO CON SOLE PAROLE IN CUI è POSSIBILE AGGIUNGERE LE DEFINIZIONI
 #include <iostream>
 #include <string>
 #include <fstream>
+#include <Windows.h>
 #define dim 116878
 
 
@@ -16,22 +17,63 @@ struct vocabolario
 {
     string parola;
     string definizione;
-};
+};//dichiarazione della struct da usare nella ram
 
 vocabolario voca[116878];
 
-void fill_ram()
-{
-    int i = 0;
-    string appoggio;
-    string stop=" *";
 
-    ifstream fill("parole.txt", ios::in);
-    while (getline(fill, appoggio))
+void ordina_file()
+{
+    int q=0, d;
+    string appoggio;
+
+    for (d = 0; d < dim; d++)
     {
-        voca[i].parola = appoggio;
-        i++;
+        voca[d].definizione = "Nessuna definizione";
     }
+
+    ifstream leggi("parole.txt", ios::in);//apro il file in lettura
+    while (getline(leggi, appoggio))//finchè posso fare la cin
+    {
+        voca[q].parola = appoggio;//riempo struct parola
+        q++;
+    }
+    leggi.close();
+
+    ofstream write("parole.txt", ios::out | ios::trunc);
+    for (d = 0; d < dim; d++)
+    {
+        write << voca[d].parola<<endl;
+        write << voca[d].definizione<<endl;
+
+        if (d == dim)
+        {
+            write << voca[d].parola << endl;
+            write << voca[d].definizione;
+        }
+    }
+    write.close();
+}
+
+void fill_ram()//funzione per riempire la struct precedentemente dichiarata
+{
+
+    int a = 0;
+    string appoggio4;
+
+    ifstream fill("parole.txt", ios::in);//apro il file in lettura
+    while (getline(fill, appoggio4))//finchè posso fare la cin
+    {
+        voca[a].parola = appoggio4;//riempo struct parola
+        
+        while (getline(fill, appoggio4))
+        {
+            voca[a].definizione = appoggio4;
+            //a++;
+        }
+        
+    }
+
     fill.close();
 }
 
@@ -62,104 +104,53 @@ int return_main()//funzione che riporta l'utente al main(inutile)
     } while (interno != 0);
 
     return interno;
-};
+}
 
 void scorrimento()//funzione che apre l'intero vocabolario a monitor
 {
-    int interno, i=0;
-  
+    int interno, i = 0;
+
     for (i = 0; i < dim; i++)
     {
         cout << voca[i].parola << endl;
         cout << voca[i].definizione << endl;
     }
     
+    cout << endl;
+}
 
-    do
-    {
-        interno = return_main();
-
-    } while (interno != 0);
-
-};
-
-string ricerca()//funzione per la ricerca di una parola
+void ricerca()
 {
-    string searched, storage;
-    int count;
+    int x = 0, y = dim - 1, i, pos = -1;
+    string temp, searched;
 
-    ofstream ricerca("deep_search.txt", ios::out | ios::trunc);//apro un file vuoto per filtrare le parole
-    ricerca.close();
-
+    cout << "Inserire la parola da cercare" << endl;
+    cin >> searched;
 
     do
     {
-        count = 0;//numero per l'utente
-        cout << "Inserire la parola cercata" << endl;
-        cin >> searched;//parola da cercare
-
-        cout << "Le parole simili alla tua ricerca sono:" << endl;
-        ifstream voci("parole.txt", ios::in);
-        while (getline(voci, storage))//finchè è aperto il buffer di lettura
+        i = (x + y) / 2;
+        if (voca[i].parola == searched)
         {
-            voci >> storage;//rovescia il database nella ram
-
-            if ((storage.find(searched) != string::npos))//se nella stringa ram c'è una porzione di parola cercata
-            {
-                cout << count + 1 << "." << storage << endl;//mostra tutte le parole numerate
-                count++;
-
-                ofstream deep("deep_search.txt", ios::out | ios::app);//apri il file svuotato precedentemente
-                deep << storage << endl;//e metti le parole filtrate
-                deep.close();  
-            }
-        }
-        voci.close();
-
-        ifstream deep2("deep_search.txt", ios::in);
-        while (getline(deep2, storage))
-        {
-            deep2 >> storage;
-            if (storage == searched)
-            {
-                return storage;
-            }
+            pos = i;
         }
 
-        /*if (count > 1)
+        else if (voca[i].parola < searched)
         {
-            cout << "Inserire il numero della parola cercata" << endl;
-            cin >> scelta;
-            
-            i = 1;
-            ifstream deep2("deep_search.txt", ios::in);
-            while(i != scelta)
-            {
-                while (getchar() != '\n')
-                {
-                    deep2 >> storage_clean;
-                }
-
-                if (i = scelta)
-                {
-                    return storage_clean;
-                }
-                i++;
-            }
-            deep2.close();
-        }*/
-
-        if (count == 0)
-        {
-            char err = 7;
-            cout << err;
-            cout << err << "La parola cercata non e' presente nell'archivio prova ad invertire il genere " << endl;
+            x = i + 1;
         }
 
+        else if (voca[i].parola > searched)
+        {
+            y = i - 1;
+        }
 
-    } while (count != 0);
+    } while (x <= y && pos == -1);
 
-};
+    cout << "La parola cercata è: " << voca[i].parola << endl;
+    cout << "La definizione presente in archivio è: " << voca[i].definizione << endl;
+
+}
 
 void inserisci_definizione()
 {
@@ -167,42 +158,108 @@ void inserisci_definizione()
     int i;
 
     cout << "Inserire la parola in cui aggiungere la definizione" << endl;
-    cin >> word_insert;
+    cin >> word_insert;//parola scelta dall'utente
 
-    for (i = 0; i <= dim; i++)
+    for (i = 0; i < dim; i++)
     {
-        if (word_insert == voca[i].parola)
+        if (word_insert == voca[i].parola)//quando la parola è presente nella struct
         {
             cout << "Inserire definizione per " << voca[i].parola << endl;
             cout << " :" << endl;
             cin.ignore();
-            getline(cin, definition);
-            voca[i].definizione = definition;
+            getline(cin, definition);//l'utente ne inserisce la definizione
+            voca[i].definizione = definition;//la definizione finiscce nella struct
         }
     }
 
-    if ((i == dim) && (word_insert != voca[i].parola))
+    if ((i == dim) && (word_insert != voca[i].parola))//se l'indice è al massimo e la parola inserita non è stata trovata
     {
-        cout << "Parola non presente nell'elenco" << endl;
+        cout << "Parola non presente nell'elenco" << endl;//errore
     }
 
-    ofstream upda_te("parole.txt", ios::out, ios::trunc);//MODIFICARE NOME FILE
+    ofstream upda_te("parole.txt", ios::out | ios::trunc);//apri file in scrittura
 
-    for (i = 0; i < dim; i++)
+    for (i = 0; i < dim; i++)//inserisci al suo interno tutta la struct con le modifiche
     {
-        upda_te << voca[i].parola<<"\t";
-        upda_te << voca[i].definizione  << endl;
+        upda_te << voca[i].parola<<endl;
+        upda_te << voca[i].definizione<<endl;
+
+        if (i == dim)
+        {
+            upda_te << voca[i].parola << endl;
+            upda_te << voca[i].definizione;
+        }
     }
     upda_te.close();
 
-    fill_ram();
-
-    main();
 }
 
-void modifica(string da_modificare)
+void modifica()
 {
+    char scelta;
+    string da_modificare, modificata;
+    int j;
 
+    cout << "vuoi modificare una parola o una definizione? p:parola - d:definizione" << endl;//cosa modifica l'utente?
+    cin >> scelta;
+
+    switch (scelta)//scelta utente
+    {
+
+    case 'd':
+    case 'D':
+        cout << "Definizione di quale parola?" << endl;
+        cin >> da_modificare;//identifico la parola a cui dare la modifica
+
+        for (j = 0; j <= dim; j++)
+        {
+            if (voca[j].parola == da_modificare)//quajndo ho trovato la parola
+            {
+                cout << "vecchia definizione di: " << voca[j].parola << endl;
+                cout << voca[j].definizione;//faccio vedere all'utente i dati vecchi
+                Sleep(1500);
+                cout << "inserire nuova definizione di: " << voca[j].parola << endl;
+                cin.ignore();//utente inserisce dati nuovi
+                getline(cin, modificata);
+                voca[j].definizione = modificata;
+                cout << voca[j].parola << endl << voca[j].definizione;
+
+                Sleep(3000);
+                main();
+            }
+        }
+
+        break;
+
+    case 'p':
+    case 'P'://uguale al case d ma per le parole
+        cout << "Parola da modificare?" << endl;
+        cin >> da_modificare;
+
+        for (j = 0; j <= dim; j++)
+        {
+            if (voca[j].parola == da_modificare)
+            {
+                cout << "modifica: " << voca[j].parola << endl;
+                Sleep(1500);
+                cout << "inserire nuova parola: " << endl;
+                cin >> modificata;
+                voca[j].parola = modificata;
+                cout << voca[j].parola << endl << voca[j].definizione;
+
+                Sleep(3000);
+                main();
+            }
+        }
+
+        break;
+
+    default:
+        char err = 7;
+        cout << err;
+        cout << err << "Scelta non consentita" << endl;
+        break;
+    }
 }
 
 
@@ -211,14 +268,17 @@ int main()
     int scelta;
     string parola;
 
-    fill_ram();
-
+    //fill_ram();
+    
+ 
     do
     {
+        fill_ram();
         cout << "1.Scorri la lista" << endl;
         cout << "2.Cerca parola" << endl;
         cout << "3.Inserisci definizione" << endl;
         cout << "4.Modifica" << endl;
+        cout << "5.Aggiorna file vocabolario" << endl;
         cin >> scelta;
 
         switch (scelta)
@@ -228,9 +288,7 @@ int main()
                 break;
 
             case 2:
-                parola = ricerca();
-                cout << endl << endl;
-                cout <<"La parola cercata e': "<< parola << endl;
+                ricerca();
                 break;
 
             case 3:
@@ -238,11 +296,16 @@ int main()
                 break;
 
             case 4:
+                modifica();
+                break;
 
+            case 5:
+                ordina_file();
                 break;
 
             default:
                 cout << "Scelta errata" << endl;
+                break;
         }
 
     } while (scelta != 0);
